47bc5330da0d46d12442262b7cc214a4b5edf04e
Gravitasi <Gravitasi@users.noreply.github.com>
2015-01-30 20:54:31 +0700

    Changed title name.

--- Source/Planet_GUI.cs
+++ Source/Planet_GUI.cs
@@ -192,7 +192,7 @@ namespace PFUtilityAddon
 		{
 			if( GuiEnabled )
 			{
-				windowPosMain = GUI.Window( 1661266, windowPosMain, WindowFunction, "Planet Editor (KopernicusHack version)" );
+				windowPosMain = GUI.Window( 1661266, windowPosMain, WindowFunction, "KittopiaTech Planet Creator v0.1" );
 			}
 			if( isshowingColourEditor )
 			{
