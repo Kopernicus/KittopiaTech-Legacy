fbc8207846d35508a40859a644e69a5f55ea42f0
Thomas <dorianstoll@googlemail.com>
2015-12-14 17:41:31 +0100

    Fixed Particle Editor

--- Source/Windows/Editors/ParticlesEditor.cs
+++ Source/Windows/Editors/ParticlesEditor.cs
@@ -1,6 +1,7 @@
 ﻿using Kopernicus.Configuration;
 using System;
 using System.IO;
+using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
 using Kopernicus.Components;
@@ -15,8 +16,11 @@ namespace Kopernicus
             // GUI stuff
             private static Vector2 scrollPosition;
 
-            // Ring
-            public static PlanetParticleEmitter particles;
+            // Particle
+            public static PlanetParticleEmitter particle;
+
+            private static List<GameObject> particles = null;
+            private static int index = 0;
 
             // Return an OnGUI()-Window.
             public static void Render()
@@ -31,112 +35,63 @@ namespace Kopernicus
                     return;
                 }
 
+                // Get the list
+                if (particles == null)
+                {
+                    particles = new List<GameObject>();
+                    foreach (Transform t in PlanetUI.currentBody.scaledBody.transform)
+                        if (t.name.Contains("Particles")) particles.Add(t.gameObject);
+                }
+                particle = particles[index].GetComponent<PlanetParticleEmitter>();
+
                 // Render the Window
-                scrollPosition = GUI.BeginScrollView(new Rect(10, 300, 400, 250), scrollPosition, new Rect(0, 280, 380, 330));
+                scrollPosition = GUI.BeginScrollView(new Rect(10, 300, 400, 250), scrollPosition, new Rect(0, 280, 380, 390));
 
-                // Ring-Selector
-                if (PlanetUI.currentBody.scaledBody.GetComponents<PlanetParticleEmitter>().Length == 0)
+                // Particle-Selector
+                if (index > 0)
                 {
-                    if (GUI.Button(new Rect(60, offset, 250, 20), "Add Particle-System"))
+                    if (GUI.Button(new Rect(20, offset, 30, 20), "<<"))
+                        index--;
+                }
+                if (GUI.Button(new Rect(60, offset, 250, 20), "Add new Particles"))
+                {
+                    // Add a new Particle
+                    particle = PlanetParticleEmitter.Create(PlanetUI.currentBody.scaledBody);
+
+                    // Get all particles
+                    particles = new List<GameObject>();
+                    foreach (Transform t in PlanetUI.currentBody.scaledBody.transform)
+                        if (t.name.Contains("Particles")) particles.Add(t.gameObject);
+                    index = particles.Count - 1;
+                }
+                if (index < particles.Count - 1)
+                {
+                    if (GUI.Button(new Rect(320, offset, 30, 20), ">>"))
+                        index++;
+                }
+                offset += 35;
+                if (particles.Count > 0)
+                {
+                    // Loop through all the Fields
+                    object[] objects = Utils.GetInfos<FieldInfo>(particle);
+                    object obj = particle as System.Object;
+                    Utils.RenderSelection<FieldInfo>(objects, ref obj, ref offset);
+                    offset += 20;
+
+                    if (GUI.Button(new Rect(20, offset, 200, 20), "Rebuild Particles"))
                     {
-                        particles = PlanetUI.currentBody.scaledBody.AddComponent<PlanetParticleEmitter>();
-                        typeof(PlanetParticleEmitter).GetMethod("Awake", BindingFlags.Instance | BindingFlags.NonPublic).Invoke(particles, null);
+                        typeof(PlanetParticleEmitter).GetMethod("Start", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(particle, null);
+                    }
+                    offset += 25;
+                    if (GUI.Button(new Rect(20, offset, 200, 20), "Delete particles on: " + PlanetUI.currentName))
+                    {
+                        UnityEngine.Object.Destroy(particles[index]);
+
+                        // Refresh the Ring-List
+                        particles = null;
+                        index = 0;
                     }
                 }
-                else
-                {
-                    particles = PlanetUI.currentBody.scaledBody.GetComponent<PlanetParticleEmitter>();
-
-                    GUI.Label(new Rect(20, offset, 178, 20), "Target");
-                    if (GUI.Button(new Rect(200, offset, 170, 20), "Edit Target"))
-                    {
-                        FieldInfo key = particles.GetType().GetField("target");
-                        CBBrowser.SetEditedObject(key, particles);
-                    }
-                    offset += 25;
-
-                    GUI.Label(new Rect(20, offset, 178, 20), "minEmission");
-                    particles.minEmission = Single.Parse(GUI.TextField(new Rect(200, offset, 170, 20), "" + particles.minEmission));
-                    offset += 25;
-
-                    GUI.Label(new Rect(20, offset, 178, 20), "maxEmission");
-                    particles.maxEmission = Single.Parse(GUI.TextField(new Rect(200, offset, 170, 20), "" + particles.maxEmission));
-                    offset += 25;
-
-                    GUI.Label(new Rect(20, offset, 178, 20), "lifespanMin");
-                    particles.minEnergy = Single.Parse(GUI.TextField(new Rect(200, offset, 170, 20), "" + particles.minEnergy));
-                    offset += 25;
-
-                    GUI.Label(new Rect(20, offset, 178, 20), "lifespanMax");
-                    particles.maxEnergy = Single.Parse(GUI.TextField(new Rect(200, offset, 170, 20), "" + particles.maxEnergy));
-                    offset += 25;
-
-                    GUI.Label(new Rect(20, offset, 178, 20), "sizeMin");
-                    particles.emitter.minSize = Single.Parse(GUI.TextField(new Rect(200, offset, 170, 20), "" + particles.emitter.minSize));
-                    offset += 25;
-
-                    GUI.Label(new Rect(20, offset, 178, 20), "sizeMax");
-                    particles.emitter.maxSize = Single.Parse(GUI.TextField(new Rect(200, offset, 170, 20), "" + particles.emitter.maxSize));
-                    offset += 25;
-
-                    GUI.Label(new Rect(20, offset, 178, 20), "speedScale");
-                    particles.speedScale = Single.Parse(GUI.TextField(new Rect(200, offset, 170, 20), "" + particles.speedScale));
-                    offset += 25;
-
-                    GUI.Label(new Rect(20, offset, 178, 20), "rate");
-                    particles.animator.sizeGrow = Single.Parse(GUI.TextField(new Rect(200, offset, 170, 20), "" + particles.animator.sizeGrow));
-                    offset += 25;
-
-                    GUI.Label(new Rect(20, offset, 200, 20), "randVelocity");
-                    Vector3 value = particles.randomVelocity;
-                    value.x = Single.Parse(GUI.TextField(new Rect(200, offset, 50, 20), "" + value.x));
-                    value.y = Single.Parse(GUI.TextField(new Rect(260, offset, 50, 20), "" + value.y));
-                    value.z = Single.Parse(GUI.TextField(new Rect(320, offset, 50, 20), "" + value.z));
-                    particles.randomVelocity = value;
-                    offset += 25;
-
-                    GUI.Label(new Rect(20, offset, 178, 20), "Texture");
-                    if (GUI.Button(new Rect(200, offset, 80, 20), "Load"))
-                    {
-                        UIController.Instance.isFileBrowser = !UIController.Instance.isFileBrowser;
-                        FileBrowser.location = "";
-                    }
-
-                    // Apply the new Texture
-                    if (GUI.Button(new Rect(290, offset, 80, 20), "Apply"))
-                    {
-                        string path = FileBrowser.location.Replace(Path.Combine(Directory.GetCurrentDirectory(), "GameData") + Path.DirectorySeparatorChar, "");
-                        Texture2D texture = Utility.LoadTexture(path, false, false, false);
-                        texture.name = path.Replace("\\", "/");
-                        particles.renderer.material.mainTexture = texture;
-                    }
-                    offset += 25;
-
-                    GUI.Label(new Rect(20, offset, 178, 20), "Colors:");
-                    offset += 25;
-
-                    int buttonOffset = 20;
-                    for (int i = 0; i < 5; i++)
-                    {
-                        if (GUI.Button(new Rect(buttonOffset, offset, 60, 20), "Color " + (i + 1)))
-                        {
-                            ColorPicker.index = i;
-                            PropertyInfo prop = typeof(ParticleAnimator).GetProperty("colorAnimation");
-                            ColorPicker.SetEditedObject(prop, particles.animator.colorAnimation[i], particles.animator);
-                        }
-                        buttonOffset += 65;
-                    }
-                    offset += 45;
-
-                    if (GUI.Button(new Rect(20, offset, 200, 20), "Delete Particles"))
-                    {
-                        MonoBehaviour.Destroy(particles.renderer);
-                        MonoBehaviour.Destroy(particles.animator);
-                        MonoBehaviour.Destroy(particles.emitter);
-                        MonoBehaviour.Destroy(particles);
-                    }
-                }
-
                 GUI.EndScrollView();
             }
         }
