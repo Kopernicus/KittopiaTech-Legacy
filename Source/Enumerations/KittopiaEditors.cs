c8146da26530a5edd7ebc44234209313b8e1911b
Dorian Stoll <dorian@tmsp.ga>
2016-05-16 19:06:33 +0200

    Enumerations

---
+++ Source/Enumerations/KittopiaEditors.cs
@@ -0,0 +1,32 @@
+﻿/** 
+ * KittopiaTech - A Kopernicus Visual Editor
+ * Copyright (c) Thomas P., BorisBee, KCreator, Gravitasi
+ * Licensed under the Terms of a custom License, see LICENSE file
+ */
+
+using System;
+
+namespace Kopernicus
+{
+    namespace UI
+    {
+        namespace Enumerations
+        {
+            /// <summary>
+            /// The editors that the mod can render
+            /// </summary>
+            public enum KittopiaEditors
+            {
+                Atmosphere,
+                CelestialBody,
+                Orbit,
+                Particles,
+                Selection,
+                Terrain,
+                Ring,
+                Starlight,
+                ScaledSpace
+            }
+        }
+    }
+}
---
+++ Source/Enumerations/KittopiaWindows.cs
@@ -0,0 +1,35 @@
+﻿/** 
+ * KittopiaTech - A Kopernicus Visual Editor
+ * Copyright (c) Thomas P., BorisBee, KCreator, Gravitasi
+ * Licensed under the Terms of a custom License, see LICENSE file
+ */
+
+using System;
+
+namespace Kopernicus
+{
+    namespace UI
+    {
+        namespace Enumerations
+        {
+            /// <summary>
+            /// The windows that the mod can render
+            /// </summary>
+            public enum KittopiaWindows
+            {
+                Planet,
+                Color,
+                LandClass,
+                Simplex,
+                NoiseMod,
+                LerpRange,
+                Biome,
+                Material,
+                Curve,
+                Selector,
+                Array,
+                Files
+            }
+        }
+    }
+}
